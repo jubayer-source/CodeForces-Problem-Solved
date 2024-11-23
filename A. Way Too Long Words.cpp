3a631b66ccb70b4832558cdbf9bc9e5ee652a3f2
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-11-23 22:52:36 +0600

    Add files via upload

---
+++ A. Way Too Long Words.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    for( int i = 0; i < n; i++)
+    {
+        string word;
+        cin >> word;
+
+        if(word.length() > 10) {
+            cout << word[0] << word.length() - 2 << word[word.length() - 1] <<endl;
+        }
+        else {
+            // Output the word as is
+            cout << word << endl;
+        }
+    }
+
+    return 0;
+}
